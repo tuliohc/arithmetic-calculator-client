bae004e2bf50cdeba5633f2bb1fc0b184fb840a9
Tulio <tuliohcosta@gmail.com>
2023-05-05 11:13:58 -0300

    add deleteRecord API call and refactor getRecords

--- src/api/records.ts
+++ src/api/records.ts
@@ -1,15 +1,54 @@
 import axios from 'axios';
+import { extractErrorMessage } from '../utils/extractErrorMessage';
 
-export const getRecords = async (): Promise<any[]> => {
+const API_URL = process.env.REACT_APP_API_URL;
+
+export interface Record {
+  _id: string;
+  user: string;
+  amount: string;
+  userBalance: string;
+  operationResponse: string;
+  deletedAt: string | null;
+  date: string;
+  operation_type: string;
+}
+
+export interface RecordsResponse {
+  page: number;
+  perPage: number;
+  totalCount: number;
+  data: Record[];
+}
+
+export interface DeleteRecordResponse {
+  date: string;
+}
+
+export const getRecords = async (page?: number, perPage?: number, search?: string): Promise<RecordsResponse> => {
   try {
-    const API_URL = process.env.REACT_APP_API_URL;
     const response = await axios.get(`${API_URL}/records`, {
+      params: {
+        page,
+        perPage,
+        search,
+      },
       withCredentials: true,
     });
 
     return response.data;
   } catch (error) {
-    console.error('Error fetching records:', error);
-    throw error;
+    throw extractErrorMessage(error, "An unknown error occurred while fetching records.");
+  }
+};
+
+export const deleteRecord = async (id: string): Promise<DeleteRecordResponse> => {
+  try {
+    const result = await axios.delete(`${API_URL}/records/${id}`, {
+      withCredentials: true,
+    });
+    return result.data
+  } catch (error) {
+    throw extractErrorMessage(error, "An unknown error occurred while deleting the record.");
   }
 };
